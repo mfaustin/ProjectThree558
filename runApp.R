c509306564825dab5d1ae0e82b01361ad62a50b9
mfaustin <doghumphrey@gmail.com>
2021-11-16 11:28:02 -0500

    added code to run app

---
+++ runApp.R
@@ -0,0 +1,17 @@
+#########################
+#  Author-Mark Austin
+#
+#  Due Date 12/05/21
+#
+#  Purpose-ST 558 
+#   Run Project 3 App
+#  
+#########################
+
+
+
+###Github changed their default from master to main but R still defaults to master so ref option needs to be added
+shiny::runGitHub('ProjectThree558',username = 'mfaustin', subdir = 'app', ref = 'main')
+
+
+
