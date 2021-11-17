10064061b844c9ac086bf4ff7537e870e70bcce1
mfaustin <doghumphrey@gmail.com>
2021-11-17 15:54:03 -0500

    added data filter code

--- app/server.R
+++ app/server.R
@@ -16,12 +16,33 @@ library(tidyverse)
 ##Read Data
 fullData<-read_excel("../data/Concrete_Data.xls")
 
+##Rename Columns to be Easier to Read
+fullData<-fullData %>% 
+  rename(Cement=`Cement (component 1)(kg in a m^3 mixture)`,
+   Blast_Furnace_Slag=`Blast Furnace Slag (component 2)(kg in a m^3 mixture)`,
+   Fly_Ash= `Fly Ash (component 3)(kg in a m^3 mixture)`,
+   Water= `Water  (component 4)(kg in a m^3 mixture)`,
+   Superplasticizer=`Superplasticizer (component 5)(kg in a m^3 mixture)`,
+   Coarse_Aggregate=`Coarse Aggregate  (component 6)(kg in a m^3 mixture)`,
+   Fine_Aggregate=`Fine Aggregate (component 7)(kg in a m^3 mixture)`,
+   Age= `Age (day)`,
+   Concrete_Compressive_Strength=`Concrete compressive strength(MPa, megapascals)`
+  )
+
+
 ##Define Shiny Server
 shinyServer(function(input, output, session){
   
+  observe({print(input$dtcolumns)})
   
-  output$tableResults<- DT::renderDataTable({fullData},
-                        options = list(scrollY = '400px',
+  subdata<-reactive({
+    subdata<-fullData %>% select(input$dtcolumns)
+    
+  })
+  
+  output$tableResults<- DT::renderDataTable({
+      newData<-subdata()},
+                        options = list(scrollY = '500px',
                                     scrollX='200px',paging=FALSE) )
   
 })
--- app/ui.R
+++ app/ui.R
@@ -19,6 +19,18 @@ library(tidyverse)
 ##Read Data
 fullData<-read_excel("../data/Concrete_Data.xls")
 
+##Rename Columns to be Easier to Read
+fullData<-fullData %>% 
+  rename(Cement=`Cement (component 1)(kg in a m^3 mixture)`,
+   Blast_Furnace_Slag=`Blast Furnace Slag (component 2)(kg in a m^3 mixture)`,
+   Fly_Ash= `Fly Ash (component 3)(kg in a m^3 mixture)`,
+   Water= `Water  (component 4)(kg in a m^3 mixture)`,
+   Superplasticizer=`Superplasticizer (component 5)(kg in a m^3 mixture)`,
+   Coarse_Aggregate=`Coarse Aggregate  (component 6)(kg in a m^3 mixture)`,
+   Fine_Aggregate=`Fine Aggregate (component 7)(kg in a m^3 mixture)`,
+   Age= `Age (day)`,
+   Concrete_Compressive_Strength=`Concrete compressive strength(MPa, megapascals)`
+  )
 
 
 ##Define UI using NavbarPage layout
@@ -31,13 +43,30 @@ shinyUI(navbarPage("Project 3",
   ##Define Project Pages with tabPanel()
   
   ##About Page Section
-  tabPanel("About"),
+  tabPanel("About",
+    ##Using fluid page for needed About elements       
+    fluidPage(
+      h2("App's Purpose"),
+      p("some sample text very long foo long foo long foo long"),
+      h2("Project Data"),
+      p("describe data"),
+      h2("App Pages"),
+      p("describe each page"),
+      p("add an image")
+    )
+  ),
   
   ##Data Page Section
   tabPanel("Data",
     fluidPage(
      sidebarLayout(
-      sidebarPanel("sidebar panel"),
+      sidebarPanel("sidebar panel",
+        checkboxInput("subdata", h4("Subset Data", style = "color:green;")),          conditionalPanel("input.subdata", 
+              checkboxGroupInput("dtcolumns", h4("Select Columns"),
+                      choices = colnames(fullData),
+                      selected = colnames(fullData))), 
+        downloadButton("downloadData", "Download Data")
+      ),
       mainPanel("main panel",
                 dataTableOutput("tableResults"))
      )  
